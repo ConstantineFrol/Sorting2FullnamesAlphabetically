47f7afd7d9b0b403701b8e4a4709117fac6575c2
Cons†antine <simplemail84@gmail.com>
2016-08-21 00:27:32 +0100

    Update Sort2FullNamesAlphabetically.java

--- Sort2FullNamesAlphabetically.java
+++ Sort2FullNamesAlphabetically.java
@@ -17,12 +17,14 @@ public class Sort2StringsAlphabetically {
 	 */
 	public static void main(String[] args) {
 		Scanner userInput = new Scanner(System.in);
-
-		String 	user1FirstName,
-				user1LastName,
-				user2FirstName,
-				user2LastName;
 		
+		// Variables that holds first and last names
+		String 	user1FirstName,
+			user1LastName,
+			user2FirstName,
+			user2LastName;
+		
+		// Prompt first and last name bouth users
 		System.out.println("Name of user 1? ");
 		
 		user1FirstName = userInput.next();
@@ -35,6 +37,7 @@ public class Sort2StringsAlphabetically {
 		
 		user2LastName = userInput.next();
 		
+		// using method .compareTo find whos name is closer to 'A' and display result
 		if(user1FirstName.compareToIgnoreCase(user2FirstName) > 0)
 		{
 			System.out.println(user2LastName + ", " + user2FirstName + "\n" + user1LastName + ", " + user1FirstName);
@@ -45,6 +48,7 @@ public class Sort2StringsAlphabetically {
 		}
 		else
 		{
+			// what if names starting with same letter, then compare their last names
 			if(user1LastName.compareToIgnoreCase(user2LastName) > 0)
 			{
 				System.out.println(user2LastName + ", " + user2FirstName + "\n" + user1LastName + ", " + user1FirstName);
